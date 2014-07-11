89d59c5369ef2785ef137c855e0b666e0b572cde
Will Earp <will.earp@icloud.com>
2014-07-11 16:52:28 +0100

    initial Class

--- Altmetric/Altmetric.py
+++ Altmetric/Altmetric.py
@@ -1,1 +1,28 @@
-# -*- coding: utf-8 -*-
+import json
+
+import requests
+
+class Altmetric:
+
+    def __init__(self, mode, timeframe=None, **kwargs):
+        self.mode = mode
+        self.timeframe = timeframe
+        self.params = kwargs 
+        self.base_url = 'http://api.altmetric.com/v1/'
+    
+    def fetch(self):
+        url = self.construct_url()
+        r = requests.get(url, params=self.params)
+        return r.json()
+           
+    def construct_url(self):
+        if self.mode == 'citations':
+            request_url = self.base_url + self.mode + '/'
+            
+            if self.timeframe:
+                request_url += self.timeframe
+
+        return request_url
+
+ 
+                
