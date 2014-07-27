a59b2d7124f177e163728217cfc515233bf1752b
Will Earp <will.earp@icloud.com>
2014-07-27 10:27:00 +0100

    deleted:    Altmetric/Altmetric.py 	deleted:    Altmetric/__init__.py 	modified:   setup.py

--- Altmetric/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from .Altmetric import Altmetric, AltmetricException, HTTPException, \
-        Citation, CitationCollection
-        
-__author__ = 'Will Earp'
-__email__ = 'will.earp@icloud.com'
-__version__ = '0.1.0'
-__all__ = ['Altmetric', 'AltmetricException', 'HttpException', 'Citation',
-        'CitationCollection']
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name='altmetric-api',
+    name='pyAltmetric',
     version='0.1',
     description='Python Altmetric API v1 wrapper',
     long_description=open('README.rst').read(),
     author='Will Earp',
     author_email='will.earp@icloud.com',
-    url='https://github.com/wearp/altmetric-api',
+    url='https://github.com/wearp/pyaltmetric',
     install_requires=['requests'],
     license="BSD",
     zip_safe=False,
