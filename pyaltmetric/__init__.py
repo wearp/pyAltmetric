63b5329e66df9797b66ac99a90a8c8af34aef35d
Will Earp <will.earp@icloud.com>
2014-07-27 10:34:31 +0100

    altmetric-api to pyaltmetric

--- pyaltmetric/__init__.py
+++ pyaltmetric/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .Altmetric import Altmetric, AltmetricException, HTTPException, \
+from .pyaltmetric import Altmetric, AltmetricException, HTTPException, \
         Citation, CitationCollection
         
 __author__ = 'Will Earp'
